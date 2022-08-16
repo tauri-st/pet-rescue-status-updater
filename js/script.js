797dff879ad842d32f28d8b1c41bc5d7d80fa001
tauri_s <tauristclaire@gmail.com>
2022-08-16 16:24:16 -0700

    Created method for play and generated pets with factory function

--- js/script.js
+++ js/script.js
@@ -2,14 +2,37 @@ const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
 
 const createPet = function (name, species) {
-    const pet {
+    const pet = {
         name: name,
         species: species,
         isTired: 5,
         sleep: function () {
             console.log(`${this.name} needs a nap. Zzz...`)
             this.isTired = 1;
+        },
+        play: function () {
+            if (this.isTired === 0) {
+                console.log("Too tired to play");
+                this.sleep();
+            }
+            else {
+                console.log(`Yay, ${this.name} loves to play!`);
+                this.isTired += 1;
+            }
         }
     };
     return pet;
 };
+
+const sora = createPet("Sora", "ferret");
+const clover = createPet("Clover", "rabbit");
+const baxter = createPet("Baxter", "hamster");
+const cleo = createPet("Cleo", "rat");
+const francine = createPet("Francine", "turtle");
+
+console.log(baxter);
+
+clover.sleep();
+baxter.play();
+
+console.log(baxter);
