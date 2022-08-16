56289d1bde7499009a6bb0d8c1b011d56569f4f8
tauri_s <tauristclaire@gmail.com>
2022-08-15 18:07:33 -0700

    Added factory function for pet and added properties

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
+
+const createPet = function (name, species) {
+    const pet {
+        name: name,
+        species: species,
+        isTired: 5,
+        sleep: function () {
+            console.log(`${this.name} needs a nap. Zzz...`)
+            this.isTired = 1;
+        }
+    };
+    return pet;
+};
