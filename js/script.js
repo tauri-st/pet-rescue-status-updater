542d43b2c62593e3ee20ebbe8d2f40b6e3ef07e2
tauri_s <tauristclaire@gmail.com>
2022-08-16 17:30:38 -0700

    Click event to display pet status on page

--- js/script.js
+++ js/script.js
@@ -5,20 +5,19 @@ const createPet = function (name, species) {
     const pet = {
         name: name,
         species: species,
-        isTired: 5,
+        isTired: 5, // Scale from 1 (refreshed) to 10 (exhausted)
         sleep: function () {
             console.log(`${this.name} needs a nap. Zzz...`)
             this.isTired = 1;
         },
         play: function () {
-            if (this.isTired === 0) {
-                console.log("Too tired to play");
+            if (this.isTired === 10) {
+                console.log("Too tired to play.");
                 this.sleep();
-            }
-            else {
+            } else {
                 console.log(`Yay, ${this.name} loves to play!`);
+                };
                 this.isTired += 1;
-            }
         }
     };
     return pet;
@@ -30,9 +29,42 @@ const baxter = createPet("Baxter", "hamster");
 const cleo = createPet("Cleo", "rat");
 const francine = createPet("Francine", "turtle");
 
-console.log(baxter);
+//console.log(baxter);
 
-clover.sleep();
-baxter.play();
+//clover.sleep();
+//baxter.play();
 
-console.log(baxter);
+//console.log(baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+//Create array of pet objects
+const allPets = [
+    sora,
+    clover,
+    baxter,
+    cleo,
+    francine
+];
+
+//Display pets in the browser
+const showPets = function (petArray) {
+    //empty list
+    pets.innerHTML = "";
+
+    for (let pet of petArray) {
+        let status = "ready to play!";
+        if (pet.isTired >= 7) {
+            status = "sleeping.";
+        }
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}.`;
+        pets.append(li);
+    }
+};
+
+//Display with click event
+statusButton.addEventListener ("click", function () {
+    showPets(allPets);
+});
